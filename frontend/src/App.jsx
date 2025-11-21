f162c4a429f93f9be4c2fb834dd53616b0603711
Julietwanza <140821704+Julietwanza@users.noreply.github.com>
2025-11-21 17:33:13 +0300

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Course from './pages/Course';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
+export default function App(){
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/course/:id' element={<Course/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
